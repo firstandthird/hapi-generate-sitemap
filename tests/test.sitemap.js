1f5fe63129afa0ce6d107086821a5adbcd6d1a8f
orthagonal <orthagonal1@gmail.com>
2017-10-20 16:33:37 -0500

    do exact match

--- tests/test.sitemap.js
+++ tests/test.sitemap.js
@@ -32,7 +32,10 @@ tap.test('generates a sitemap', (t) => {
         url: '/sitemap.html'
       }, (response) => {
         t.equal(response.statusCode, 200, 'returns HTTP OK');
-        t.notEqual(response.result.indexOf(`<a href="http://${server.info.host}:${server.info.port}/path1">http://${server.info.host}:${server.info.port}/path1</a>`), -1, 'maps server route');
+        t.equal(response.result, `
+              <ul>
+                <li><a href="http://${server.info.host}:${server.info.port}/path1">http://${server.info.host}:${server.info.port}/path1</a></li>
+              </ul>`);
         done();
       });
     }
@@ -78,7 +81,6 @@ tap.test('can take custom endpoint', (t) => {
         url: '/endpoint.html'
       }, (response) => {
         t.equal(response.statusCode, 200, 'returns HTTP OK');
-        t.notEqual(response.result.indexOf(`<a href="http://${server.info.host}:${server.info.port}/path1">http://${server.info.host}:${server.info.port}/path1</a>`), -1, 'maps server route');
         done();
       });
     }
@@ -134,8 +136,10 @@ tap.test('ignore routes by routetag', (t) => {
         url: '/sitemap.html'
       }, (response) => {
         t.equal(response.statusCode, 200, 'returns HTTP OK');
-        t.notEqual(response.result.indexOf(`<a href="http://${server.info.host}:${server.info.port}/path1">http://${server.info.host}:${server.info.port}/path1</a>`), -1, 'maps server route');
-        t.equal(response.result.indexOf(`<a href="http://${server.info.host}:${server.info.port}/redirect">http://${server.info.host}:${server.info.port}/redirect</a>`), -1, 'excluded routes not mapped');
+        t.equal(response.result.trim(),
+        `<ul>
+                <li><a href="http://${server.info.host}:${server.info.port}/path1">http://${server.info.host}:${server.info.port}/path1</a></li>
+              </ul>`);
         done();
       });
     }
@@ -193,8 +197,9 @@ tap.test('ignore routes by plugin config on route', (t) => {
         url: '/sitemap.html'
       }, (response) => {
         t.equal(response.statusCode, 200, 'returns HTTP OK');
-        t.notEqual(response.result.indexOf(`<a href="http://${server.info.host}:${server.info.port}/path1">http://${server.info.host}:${server.info.port}/path1</a>`), -1, 'maps server route');
-        t.equal(response.result.indexOf(`<a href="http://${server.info.host}:${server.info.port}/redirect">http://${server.info.host}:${server.info.port}/redirect</a>`), -1, 'excluded routes not mapped');
+        t.equal(response.result.trim(), `<ul>
+                <li><a href="http://${server.info.host}:${server.info.port}/path1">http://${server.info.host}:${server.info.port}/path1</a></li>
+              </ul>`);
         done();
       });
     }
@@ -254,8 +259,9 @@ tap.test('accepts a function containing additional unlisted routes', (t) => {
         url: '/sitemap.html'
       }, (response) => {
         t.equal(response.statusCode, 200, 'returns HTTP OK');
-        t.equal(response.result.indexOf(`<a href="http://${server.info.host}:${server.info.port}add1">http://${server.info.host}:${server.info.port}add1</a>`), -1, 'addition routes mapped');
-        t.equal(response.result.indexOf(`<a href="http://${server.info.host}:${server.info.port}add2">http://${server.info.host}:${server.info.port}add2</a>`), -1, 'addition routes mapped');
+        t.equal(response.result.trim(), `<ul>
+                <li><a href="http://${server.info.host}:${server.info.port}/add1">http://${server.info.host}:${server.info.port}/add1</a></li><li><a href="http://${server.info.host}:${server.info.port}/add2">http://${server.info.host}:${server.info.port}/add2</a></li><li><a href="http://${server.info.host}:${server.info.port}/path1">http://${server.info.host}:${server.info.port}/path1</a></li>
+              </ul>`);
         done();
       });
     }
@@ -267,7 +273,7 @@ tap.test('accepts a function containing additional unlisted routes', (t) => {
   });
 });
 
-tap.test('can also return txt and xml output', (t) => {
+tap.test('can also return txt output', (t) => {
   async.autoInject({
     server(done) {
       const server = new Hapi.Server();
@@ -284,8 +290,49 @@ tap.test('can also return txt and xml output', (t) => {
       });
       done();
     },
-    autoHandler(server, done) {
-      server.register(require('hapi-auto-handler'), {}, done);
+    sitemap(server, done) {
+      server.register({
+        register: plugin,
+        options: {}
+      }, done);
+    },
+    start(server, sitemap, done) {
+      server.start(done);
+    },
+    testTxt(start, server, done) {
+      server.inject({
+        method: 'get',
+        url: '/sitemap.txt'
+      }, (response) => {
+        t.equal(response.statusCode, 200, 'returns HTTP OK');
+        t.equal(response.result, `http://${server.info.host}:${server.info.port}/path1`, 'returns txt map');
+        done();
+      });
+    }
+  }, (err, results) => {
+    t.equal(err, null);
+    results.server.stop(() => {
+      t.end();
+    });
+  });
+});
+
+tap.test('can also return xml output', (t) => {
+  async.autoInject({
+    server(done) {
+      const server = new Hapi.Server();
+      server.connection({ port: 8080 });
+      return done(null, server);
+    },
+    routes(server, done) {
+      server.route({
+        method: 'get',
+        path: '/path1',
+        handler(request, reply) {
+          return reply(null, { success: true });
+        }
+      });
+      done();
     },
     sitemap(server, done) {
       server.register({
@@ -306,16 +353,6 @@ tap.test('can also return txt and xml output', (t) => {
         done();
       });
     },
-    testTxt(start, server, done) {
-      server.inject({
-        method: 'get',
-        url: '/sitemap.txt'
-      }, (response) => {
-        t.equal(response.statusCode, 200, 'returns HTTP OK');
-        t.equal(response.result, `http://${server.info.host}:${server.info.port}/path1`, 'returns txt map');
-        done();
-      });
-    }
   }, (err, results) => {
     t.equal(err, null);
     results.server.stop(() => {
