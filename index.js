9e7312b56e9d0df419f4565f303ed477bef0e0a0
orthagonal <orthagonal1@gmail.com>
2018-02-22 17:36:23 -0600

    typo?

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const register = (server, pluginOptions) => {
           </ul>`;
         return html;
       } else if (request.params.type === 'xml') {
-        const xml = `;
+        const xml = `
           <?xml version="1.0" encoding="UTF-8"?>
           <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
             ${all.map((url) => `<url><loc>${protocol}://${request.info.host}${url}</loc></url>`).join('')}
