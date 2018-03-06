f5f622ea848df95372dc9c66a87c4aac7c471428
orthagonal <orthagonal1@gmail.com>
2018-03-06 13:54:55 -0600

    getRouteMetaData (#22)
    * getRouteMetaData
    
    * move, improve test
    
    * no comma
    
    * update

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const register = (server, pluginOptions) => {
     excludeUrls: Joi.array().default([]), // these urls are excluded from the sitemap
     additionalRoutes: Joi.func().optional(), // returns any additional routes you want listed on your sitemap
     dynamicRoutes: Joi.func().optional(), //  returns what routes to list for routes that use dynamic params (eg '/user/{userName}' --> ['/user/roberto', '/user/maria'])
+    getRouteMetaData: Joi.func().optional(), // returns any metadata you want associated with a given route
     endpoint: Joi.string().default('/sitemap'), // name of the path where you can GET a copy of the sitemap
     logRequest: Joi.boolean().default(false) // specify whether to log each request for the sitemap
   }));
--- tests/test.sitemap.js
+++ tests/test.sitemap.js
@@ -703,6 +703,45 @@ tap.test('lastmod, changefreq and priority sitemap tags can be set by dynamicRou
   t.end();
 });
 
+tap.test('lastmod, changefreq and priority sitemap tags can be set by a custom function (getRouteMetaData)', async(t) => {
+  const server = await new Hapi.Server({ port: 8080 });
+  await server.register({
+    plugin,
+    options: {
+      getRouteMetaData(page) {
+        t.match(page.path, '/path1');
+        return {
+          lastmod: '2005-01-01',
+          priority: 0.8,
+        };
+      }
+    }
+  });
+  server.route({
+    method: 'get',
+    path: '/path1',
+    config: {
+      plugins: {
+        sitemap: {
+          changefreq: 'monthly',
+        }
+      }
+    },
+    handler(request, h) {
+      return 'hello';
+    }
+  });
+  await server.start();
+  const response = await server.inject({
+    method: 'get',
+    url: '/sitemap.xml'
+  });
+  t.equal(response.statusCode, 200, 'returns HTTP OK');
+  t.match(response.result, `<url><loc>http://${server.info.host}:${server.info.port}/path1</loc><lastmod>2005-01-01</lastmod><changefreq>monthly</changefreq><priority>0.8</priority></url>`, 'formats sitemap tags correctly');
+  await server.stop();
+  t.end();
+});
+
 tap.test('.json?meta=1 will also return metadata (section, lastmod, changefreq, priority) ', async(t) => {
   const server = await new Hapi.Server({ port: 8080 });
   await server.register(plugin, {});
